7f844abab126bce1bffa59c89312134b39a11920
marekmitko <mitko.email@gmail.com>
2022-02-24 16:03:00 +0100

    init repo - app/invoice-beta/

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { Admin } from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server';
+
+
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Admin dataProvider={dataProvider} />
     </div>
   );
 }
