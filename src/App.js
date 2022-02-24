785f56967dd2a1b5a8723e40f1e46c6343c0679f
marekmitko <mitko.email@gmail.com>
2022-02-24 16:33:03 +0100

    Making Contact - API using DataProvider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import * as React from "react";
 import { Admin } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
-
-
+//Making Contact With The API Using a Data Provider
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
-// działa 
 function App() {
   return (
     <div className="App">
